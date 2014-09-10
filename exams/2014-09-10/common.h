b5e396e28cbfe7934f74c629562c1549719f85c0
Luca Ghio <artghio@tiscali.it>
2014-09-10 19:57:52 +0200

    Exam 2014-09-10 - added forgotten file

---
+++ exams/2014-09-10/common.h
@@ -0,0 +1,15 @@
+/** @file
+  * @brief Contiene eventuali risorse da condividere al volo tra il client e il server.
+  */
+
+#ifndef	__common_h
+#define	__common_h
+
+#define MAXFILENAMELENGTH MAXLINE
+
+#define PUT "PUT"
+#define QUIT "QUIT\r\n"
+#define OK "+OK\r\n"
+#define ERR "-ERR\r\n"
+
+#endif
